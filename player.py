6934c7ccf44b36f1624173ce0df99485fe587e49
Bud <shenen4@gmail.com>
2023-09-24 15:57:13 +1000

    fixing writing logic backend

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import random
 import numpy
 import pandas as pd
-from datetime import date
+from datetime import date, datetime
 
 headers = {
     "Host": "stats.nba.com",
@@ -73,32 +73,33 @@ def get_player_by_full_name(player_full_name):
 
 def get_daily_player():
     # Check the current date against the stored date in time.txt
-    stored_time = open("time.txt", "r+")
-    stored_player = open("player.txt", "r+")
-    stored_time_value = stored_time.readline()
-    stored_player_value = stored_player.readline()
+    stored_time_read = open("time.txt", "r")
+    stored_player_read = open("player.txt", "r")
+    stored_time_value = stored_time_read.readline()
+    stored_player_value = stored_player_read.readline()
+    stored_time_read.close()
+    stored_player_read.close()
 
     # If the current date equals stored date, return stored player data
     if str(date.today()) == stored_time_value:
-        stored_time.close()
-        stored_player.close()
+        print("im here")
         return get_player_by_full_name(stored_player_value)
     # If the current date is greater than the stored date
-    elif str(date.today()) > stored_time_value:
+    elif date.today() > datetime.strptime(stored_time_value, "%Y-%m-%d").date():
+        print("elif")
         # Get a new player from top_100_players_id.csv and get their data
         new_player = get_player()
         if new_player["full_name"] == stored_player_value:
             new_player = get_player()
         # Change time.txt to the new current time
-        stored_time.truncate(0)
-        stored_time.write(str(date.today()))
+        stored_time_write = open("time.txt", "w")
+        stored_time_write.write(str(date.today()))
+        stored_time_write.close()
         # Change player.txt to the new player's name
-        stored_player.truncate(0)
-        stored_player.write(new_player["full_name"])
-
+        stored_player_write = open("player.txt", "w")
+        stored_player_write.write(new_player["full_name"])
+        stored_player_write.close()
         # Return the player's data
-        stored_time.close()
-        stored_player.close()
         return new_player
 
 
